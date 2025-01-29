eb2b3a85470bfd012e42e93c560c24eaf066c63b
bishalhota <bishalhota264@gmail.com>
2025-01-29 06:20:01 +0530

    signup end to end finish

--- Brainzy-frontend/src/pages/Signup.tsx
+++ Brainzy-frontend/src/pages/Signup.tsx
@@ -9,25 +9,24 @@ export function Signup() {
     const usernameRef = useRef<HTMLInputElement>();
     const passwordRef = useRef<HTMLInputElement>();
 
-    async function signup(){
+    async function signup() {
         const username = usernameRef.current?.value;
         const password = passwordRef.current?.value;
-        await axios.post(BACKEND_URL + "/api/v1/signup",{
-            data:{
-                username:username,
-                password:password
-            }
-        })
-    }
+        await axios.post(BACKEND_URL + "/api/v1/signup", {
+            username: username,
+            password: password
+        }
+        );
+}
 
-    
-    return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
-        <div className="bg-white rounded border min-2-48">
-            <Input reference={usernameRef} placeholder="Username" />
-            <Input reference={passwordRef} placeholder="Password" />
-            <div className="flex justify-center pt-4">
-                <Button variant="primary" size="md" text="Signup" fullwidth={true} onClick={signup} />
-            </div>
+
+return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
+    <div className="bg-white rounded border min-2-48">
+        <Input reference={usernameRef} placeholder="Username" />
+        <Input reference={passwordRef} placeholder="Password" />
+        <div className="flex justify-center pt-4">
+            <Button variant="primary" size="md" text="Signup" fullwidth={true} onClick={signup} />
         </div>
     </div>
+</div>
 }
